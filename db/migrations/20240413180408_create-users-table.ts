2319daf9857166cef158a22bc9ac2a1314c6629c
Christian Rodrigues <crrodrigues2001@outlook.com.br>
2024-04-17 19:41:13 -0300

    feat: add unique rule to username in users table  migration

--- db/migrations/20240413180408_create-users-table.ts
+++ db/migrations/20240413180408_create-users-table.ts
@@ -1,18 +1,15 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.createTable("users", (table) => {
-        table.uuid("id").primary()
-        table.text("name").notNullable
-        table.text("password").notNullable
-        table.timestamp("createdAt").defaultTo(knex.fn.now()).notNullable()
-        table.timestamp("lastLogin")
-    })
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('name').unique().notNullable()
+    table.text('password').notNullable()
+    table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('lastLogin')
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTable("users")
+  await knex.schema.dropTable('users')
 }
-
